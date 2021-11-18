68d0fbe44af66aa40a42f7d2fd7ba25b8eac31ee
Marcusi7 <mealtamirano@puce.edu.ec>
2021-11-17 19:01:11 -0500

    u11v2

--- api/product.js
+++ api/product.js
@@ -40,6 +40,17 @@ export async function getTotalProductsCategoriaApi(categoria){
         return null;
 
       }
+}
 
 
+export async function getProductByUrlApi(path) {
+try {
+    const url = `${BASE_PATH}/products?url=${path}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    return result[0];
+} catch (error) {
+    console.log(error);
+    return null;
+}
 }
--- pages/[product].js
+++ pages/[product].js
@@ -1,13 +1,24 @@
 import React, {useState, useEffect} from "react";
 import { useRouter } from "next/router";
 import BasicLayout  from '../layouts/BasicLayout/BasicLayout';
+import { getProductByUrlApi} from "../api/product"
+
 
 export default function Product() {
+
+    const [product, setProduct] = useState(null);
     const {query} =useRouter();
+
+    useEffect(() => {
+        (async () => {
+            const response = await getProductByUrlApi(query.product);
+            setProduct(response);
+        })()
+    }, [query])
     
 
     return (
-        <BasicLayout className="game">
+        <BasicLayout className="product">
             <h1>Estamos en Product: {query.product} </h1>
         </BasicLayout>
     );
